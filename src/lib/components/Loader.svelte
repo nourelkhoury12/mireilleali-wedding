ff4405b47e95b16ae8c102a02de7192b476f1fad
nourelkhoury12 <nourkhoury201@gmail.com>
2026-07-17 22:24:21 +0300

    update the loader page

--- src/lib/components/Loader.svelte
+++ src/lib/components/Loader.svelte
@@ -1,13 +1,58 @@
 <script lang="ts">
+    import Image from "$lib/assets/logob.png"
+	const text = "Developed by Blue Project...";
+
+	let visible = $state("");
+
+	$effect(() => {
+		let i = 0;
+
+		const interval = setInterval(() => {
+			if (i < text.length) {
+				visible += text[i];
+				i++;
+			} else {
+				clearInterval(interval);
+			}
+		}, 70);
+
+		return () => clearInterval(interval);
+	});
 </script>
 
-<div class="loader">
-    <div class="track">
-        <div class="horses">
-            🐎 🐎
-        </div>
-    </div>
+<div class="flex min-h-screen flex-col items-center justify-center bg-white px-6">
 
-    <h1>Ali & Mireille</h1>
-    <p>Riding to your special day...</p>
+	<img
+		src={Image}
+		alt="Blue Project"
+		class="mb-12 h-28 w-auto object-contain md:h-36"
+	/>
+
+	<div class="mb-10 h-1 w-72 overflow-hidden rounded-full bg-slate-200 md:w-96">
+		<div class="loader-line h-full bg-blue-600"></div>
+	</div>
+
+	<h3 class="text-center text-xl text-slate-700 md:text-2xl">
+		{visible}<span class="animate-pulse">|</span>
+	</h3>
+
 </div>
+
+<style>
+	.loader-line{
+		width:0%;
+		animation:loading 2.6s linear forwards;
+	}
+
+	@keyframes loading{
+
+		from{
+			width:0%;
+		}
+
+		to{
+			width:100%;
+		}
+
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,11 +5,11 @@
 	import RSVP from '$lib/components/RSVP.svelte';
 	import MarriageList from '$lib/components/MarriageList.svelte';
 	import Footer from '$lib/components/Footer.svelte';
-
+	import Loader from '$lib/components/Loader.svelte';
 	import { onMount } from 'svelte';
 
 	let showButton = $state(false);
-
+	let loading = $state(true);
 	function scrollToTop() {
 		window.scrollTo({
 			top: 0,
@@ -18,22 +18,39 @@
 	}
 
 	onMount(() => {
+		const timer = setTimeout(() => {
+			loading = false;
+		}, 4000);
+
 		const handleScroll = () => {
 			showButton = window.scrollY > 300;
 		};
 
 		window.addEventListener('scroll', handleScroll);
 
-		return () => window.removeEventListener('scroll', handleScroll);
+		return () => {
+			clearTimeout(timer);
+			window.removeEventListener('scroll', handleScroll);
+		};
 	});
 </script>
 
-<Hero />
-<CountDown/>
-<Venue />
-<RSVP />
-<MarriageList />
-<Footer />
+{#if loading}
+	<Loader />
+{:else}
+	<Hero />
+	<CountDown />
+	<Venue />
+	<RSVP />
+	<MarriageList />
+	<Footer />
+
+	{#if showButton}
+		<button class="back-to-top" onclick={scrollToTop}>
+			↑
+		</button>
+	{/if}
+{/if}
 
 {#if showButton}
 	<button class="back-to-top" onclick={scrollToTop}>
